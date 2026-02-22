c0d5f4b58ad894c29f40abf62c8cb273c584b786
Edmond Vardanyan <edmondvar2005@gmail.com>
2026-02-22 20:13:29 +0400

    github check

--- isDigitChar.cpp
+++ isDigitChar.cpp
@@ -1,6 +1,6 @@
 // In this file, I implemented logic to check whether a character passed as an argument is numeric.
 #include "main.hpp"
-//a
+
 bool isDigit(char c){
     if (c >= '0' && c <= '9')
         return true;
